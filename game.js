17681ad362aefb5ebcb30484c1ad58feb8fd63f3
lioraspirt101 <40323766+lioraspirt101@users.noreply.github.com>
2018-06-17 11:33:30 -0400

    Update game.js

--- game.js
+++ game.js
@@ -24,6 +24,7 @@ startGame();
 function startGame(){
   document.querySelector('.endgame').style.display = 'none';
   origBoard = Array.from(Array(9).keys());
+  alert(cells.length);
   for( var i = 0; i < cells.length; i++){
     cells[i].innerText = ''; 
     cells[i].style.removeProperty('background-color');
